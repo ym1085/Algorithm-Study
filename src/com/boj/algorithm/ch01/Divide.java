711c24fd967aeb1a9f66078c3a2d6a8b1ce61b38
ym1085 <youngmin1085@gmail.com>
2022-01-22 20:21:19 +0900

    FEAT: 입출력과 사칙연산 문제풀이 완료
    1. 두 수를 입력받고 합을 출력하는 문제
    2. 두 수를 입력받고 뺄셈을 한 결과를 출력하는 문제
    3. 곱셈 문제
    4. 나눗셈 문제
    5. 모든 연산 문제
    6. 네 개의 계산식을 계산하는 문제
    7. 빈 칸에 들어갈 수는?
    8. README.md 입출력과 사칙연산 링크 연결

---
+++ src/com/boj/algorithm/ch01/Divide.java
@@ -0,0 +1,34 @@
+package com.boj.algorithm.ch01;
+
+import java.util.Scanner;
+
+/**
+ * @author : ymkim
+ * @since 20220122
+ *
+ * [문제 번호]
+ *
+ * 1008
+ *
+ * [문제]
+ * 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+ *
+ * [입력]
+ * 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+ *
+ * [출력]
+ * 첫째 줄에 A/B를 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-9 이하이면 정답이다.
+ */
+public class Divide {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        float a = sc.nextInt(); // 4byte, 7-8자리 표현
+//        float b = sc.nextInt();
+        double a = sc.nextInt(); // 8byte, 16자리 표현 (유효자리수: 정밀도)
+        double b = sc.nextInt();
+
+        // 0.3333333333333333
+        System.out.println(a / b);
+    }
+}
---
+++ src/com/boj/algorithm/ch01/Minus.java
@@ -0,0 +1,31 @@
+package com.boj.algorithm.ch01;
+
+import java.util.Scanner;
+
+/**
+ * @author : ymkim
+ * @since 20220122
+ *
+ * [문제 번호]
+ *
+ * 1001
+ *
+ * [문제]
+ * 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+ *
+ * [입력]
+ * 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+ *
+ * [출력]
+ * 첫째 줄에 A-B를 출력한다.
+ */
+public class Minus {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a - b);
+    }
+}

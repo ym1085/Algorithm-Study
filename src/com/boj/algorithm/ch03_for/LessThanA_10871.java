a18cf02e967ac33c8495a5eda9dcd48981edc9df
ym1085 <youngmin1085@gmail.com>
2022-02-05 20:22:30 +0900

    FEAT: for문 마지막 문제 풀이
    1. 클래스명에 문제번호 누락된 부분 추가
    2. 루트 README.md 파일에 해당 문제 추가

--- src/com/boj/algorithm/ch03_for/LessThanA_10871.java
+++ src/com/boj/algorithm/ch03_for/LessThanA_10871.java
@@ -25,7 +25,7 @@ import java.util.StringTokenizer;
  * [출력]
  * X보다 작은 수를 입력받은 순서대로 공백으로 구분해 출력한다. X보다 작은 수는 적어도 하나 존재한다.
  */
-public class LessThanA {
+public class LessThanA_10871 {
 
     public static void main(String[] args) throws IOException {
         /*

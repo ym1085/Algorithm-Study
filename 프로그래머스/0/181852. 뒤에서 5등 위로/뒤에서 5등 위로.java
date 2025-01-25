488511e0d4671f37a919f6dbbb4bcf17c32fb515
YOUNGMINKIM <53969142+ym1085@users.noreply.github.com>
2025-01-25 21:00:00 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.56 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -8,16 +8,18 @@ class Solution {
      */
     public int[] solution(int[] num_list) {
         
+        // 오름차순 정렬 수행
         Arrays.sort(num_list);
         
-        int idx = 0;
-        
+        // 결과를 담을 공간을 지정
+        // 10 -> 5 ==> 5개의 공간
         int[] answer = new int[num_list.length - 5];
         
-        for (int i = 5; i < num_list.length; i++) {
+        // 루프 수행
+        int idx = 0;
+        for (int i = 5; i < num_list.length; i++){
             answer[idx++] = num_list[i];
         }
-        
         return answer;
     }
 }

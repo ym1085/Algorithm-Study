b14eaedb969c232bbfaae6fc81dfae4107f99908
YOUNGMINKIM <53969142+ym1085@users.noreply.github.com>
2025-02-06 20:14:32 +0900

    [level 0] Title: 배열 만들기 1, Time: 7.14 ms, Memory: 89.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int n, int k) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % k == 0) {
+                answer.add(i);
+            }
+        }
+        return answer;
+    }
+}
